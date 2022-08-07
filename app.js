82f12231d6056337f0805c66e4270e52be440d86
Benson Jing <bensonjing23@gmail.com>
2022-08-07 12:52:19 -0400

    setup mongoDB connection

--- app.js
+++ app.js
@@ -2,12 +2,22 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+import "dotenv/config";
+import cors from "cors";
+import mongoose from "mongoose";
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 
+// setup mongoDB connection
+const mongoDB = process.env.MONGODB_URI;
+mongoose.connect(mongoDB, { useUnifiedTopology: true, useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "mongo connection error"));
+
 var app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
